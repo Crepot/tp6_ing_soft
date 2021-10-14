db8742679abec6269523467f43197fe243519187
Crepot <fjgallardo97@gmail.com>
2021-10-14 16:19:53 -0300

    -

--- test/test001.js
+++ test/test001.js
@@ -7,7 +7,7 @@ describe('Stock functions: \n', function(){
     let result;
     it('checkStock should return code: 200 and status: on stock ',function(){
         result = stock.checkStock(3,10);
-        assert.isObject(result,'FAIL');
+        assert.isObject(result,'result is not an object');
     })
     it('addStock should return code: 200 and total stock units ',function(){
         result = stock.addStock(3,10);
