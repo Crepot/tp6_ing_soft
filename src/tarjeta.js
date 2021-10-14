bb17ce8abc33fd966425756581915a8d02d8006d
Martin <mlmazzeo96@gmail.com>
2021-10-14 17:42:45 -0300

    s

--- src/tarjeta.js
+++ src/tarjeta.js
@@ -1,25 +1,34 @@
-function checkSecurityCodeLength(codigo){
-    let result = codigo.toString().length;
-    if((result == 3) || (result == 4)){
-        return true;
-    }else{
-        return false;
-    }
+//JuanTroncoso
+function checkSecurityCodeLength(codigo) {
+  let result = codigo.toString().length;
+  if (result == 3 || result == 4) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function checkCardNumberLength(card){
-    let result = card.toString().length;
-    if(result == 16){
-        return true;
-    }else{
-        return false;
-    }}
-    function checkCardNumber(cardNumber) {
-        if (cardNumber.match(/^[0-9]+$/)) {
-          return true;
-        } else {
-          return false;
-        }
-      }
+//JuanTroncoso
+function checkCardNumberLength(card) {
+  let result = card.toString().length;
+  if (result == 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-module.exports ={checkSecurityCodeLength,checkCardNumber,checkCardNumberLength}
+//AyrtonFunes
+function checkCardNumber(cardNumber) {
+  if (cardNumber.match(/^[0-9]+$/)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = {
+  checkSecurityCodeLength,
+  checkCardNumber,
+  checkCardNumberLength,
+};
