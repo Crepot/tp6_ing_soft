6f0d39a6fad04935a4b05c437b2860b316e85ec9
Martin <mlmazzeo96@gmail.com>
2021-10-14 17:41:42 -0300

    Terminadas pruebas unitaras. Martin Mazzeo

--- src/carrito.js
+++ src/carrito.js
@@ -1,3 +1,4 @@
+//AyrtonFunes
 function checkQuantity(cant) {
   if (cant.match(/^[0-9]+$/)) {
     return true;
@@ -5,4 +6,20 @@ function checkQuantity(cant) {
     return false;
   }
 }
-module.exports = { checkQuantity };
+
+//MartinMazzeo
+function verificarNumeroEntero(num) {
+  if (num > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//MartinMazzeo
+function verificarPrecioTotal(cant, precio) {
+  let result = cant * precio;
+  return result;
+}
+
+module.exports = { checkQuantity, verificarNumeroEntero, verificarPrecioTotal };
